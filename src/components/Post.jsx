57911f8e72e6d08b6086ff58093d1e3d2c091d75
Maseline <mmusimire@gmail.com>
2017-05-15 17:51:58 +0300

    Add like feature

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,30 +1,59 @@
 import React from 'react';
-import '../index.css';
 import profilePhoto from '../images/NBC-news.png';
 import myPost from '../images/teen-reporters.jpg';
+import Likes from './Likes';
+import '../index.css';
+import '../bootstrap/bootstrap.css';
 
-function Post() {
-  // const profilePhoto = '../images/download.jpg';
-  // const userName = '';
-  const title = 'In LA’s Boyle Heights, Latino Teens Draw National Praise for Community Journalism';
-  // const myPost = '../images/images.jpg';
-  const datePosted = '27 mins';
-  // const timePosted = new Date().toLocaleTimeString();
-  // likes: '',
-  // comments: '',
-  return (
-    <div className="container">
-      <table cellSpacing="0">
-        <tr><td rowSpan="2">
-          <img src={profilePhoto} height="50" width="50" alt="profilePicture" />
-        </td>
-          <td>
-            <p1 className="userName">NBC News <p3 className="live">is live now.</p3></p1>
-          </td></tr><tr><td>
-            <p2 className="postDate"> {datePosted}</p2></td></tr></table>
-      <p className="postTitle">{title}</p> <br />
-      <img src={myPost} id="myPost" alt="myPost" />
-    </div>
-  );
+class Post extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: 'In LA’s Boyle Heights, Latino Teens Draw National Praise for Community Journalism',
+      datePosted: '27 mins',
+      userName: 'NBC News',
+    };
+  }
+  render() {
+    return (
+      <div className="facebook">
+        <div className="container">
+          <div className="row" id="header">
+            <div className="col-md-1">
+              <img src={profilePhoto} alt="profilePicture" height="40" width="40" />
+            </div>
+
+            <div className="col-md-4" id="userName">
+              {this.state.userName}
+              <p3 className="live"> is live now.</p3>
+              <div id="postDate">
+                {this.state.datePosted}
+              </div>
+            </div>
+          </div>
+          <div className="postTitle">
+            {this.state.title}
+          </div>
+          <br />
+          <div>
+            <img src={myPost} id="myPost" alt="myPost" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="row" id="react">
+            <div className="col-md-8">
+              <Likes />
+            </div>
+            <div className="container">
+              <div className="row" id="react">
+              </div>
+            </div>
+            <hr />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Post;
+
